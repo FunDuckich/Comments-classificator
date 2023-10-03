93e953b4748696090c949a5a1ccc3a60a9c3c8a0
FunDuckich <petrovegor495@gmail.com>
2023-10-03 20:30:45 +0400

    Вот база

---
+++ base.py
@@ -0,0 +1,74 @@
+import aiogram.fsm.state
+from aiogram import Bot, Dispatcher, Router, types, enums, F
+from aiogram.filters import CommandStart
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.context import FSMContext
+
+
+r = Router()
+types_for_comment = ['Фотографию', 'Гифку', 'Текст']
+
+
+def make_row_keyboard(items: list[str]) -> types.ReplyKeyboardMarkup:
+    row = [types.KeyboardButton(text=item) for item in items]
+    return types.ReplyKeyboardMarkup(keyboard=[row], resize_keyboard=True)
+
+
+class CommentSomething(StatesGroup):
+    choosing_type_for_comment = State()
+    commenting = State()
+
+
+@r.message(CommentSomething.choosing_type_for_comment, F.text.in_(types_for_comment))
+async def type_was_chosen(message: types.Message, state: FSMContext):
+    if message.text.lower() == 'фотографию':
+        picture_for_comment = types.FSInputFile("picture.jpeg")
+        await message.answer_photo(
+            picture_for_comment,
+            caption='Жду вашего комментария на следующую фотографию:',
+            reply_markup=types.ReplyKeyboardRemove()
+        )
+    elif message.text.lower() == 'гифку':
+        gif_for_comment = types.FSInputFile("roach-dancing.mp4")
+        await message.answer_video(
+            gif_for_comment,
+            caption='Жду вашего комментария на следующую гифку:',
+            reply_markup=types.ReplyKeyboardRemove()
+        )
+    else:
+        await message.answer(
+            text='Ну тут типа текст какой-то, который нужно будет прокомментировать.',
+            reply_markup=types.ReplyKeyboardRemove()
+        )
+    await state.set_state(CommentSomething.commenting)
+
+
+@r.message(CommentSomething.choosing_type_for_comment)
+async def type_chosen_incorrectly(message: types.Message):
+    await message.answer(
+        text="Написали же выбрать <b>тип контента</b>, просто <b>нажми на кнопку</b>!",
+        reply_markup=make_row_keyboard(types_for_comment)
+    )
+
+
+@r.message(CommentSomething.commenting)
+async def rate(message: types.Message, state: FSMContext):
+    await message.answer(
+        text='Ну тут типа будет оценка вашего комментария',
+        reply_markup=types.ReplyKeyboardRemove()
+    )
+    await state.clear()
+
+
+@r.message(CommandStart())
+async def giving_a_choice(message: types.Message, state: FSMContext):
+    await message.answer(
+        text='И так, выберите контент, который хотите прокомментировать',
+        reply_markup=make_row_keyboard(types_for_comment)
+    )
+    await state.set_state(CommentSomething.choosing_type_for_comment)
+
+
+@r.message()
+async def send_guide(message: types.Message):
+    await message.answer(f'{message.from_user.first_name}, воспользуйтесь коммандой /start, пожалуйста.')
--- main.py
+++ main.py
@@ -1,31 +1,24 @@
 import sys
 from config import API_TOKEN
+from base import r
 import asyncio
 import logging
-from aiogram import Bot, Dispatcher, Router, types, enums
-from aiogram.filters import CommandStart
-
-dp = Dispatcher()
-
-
-@dp.message(CommandStart())
-async def command_start_handler(message: types.Message) -> None:
-    await message.answer(f"Привет, {message.from_user.full_name}!")
-
-
-@dp.message()
-async def echo_handler(message: types.Message) -> None:
-    try:
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        await message.answer("Хорошая попытка, фрик")
+from aiogram import Bot, Dispatcher, enums
 
 
 async def main() -> None:
     bot = Bot(API_TOKEN, parse_mode=enums.ParseMode.HTML)
+    dp = Dispatcher()
+    dp.include_router(router=r)
+
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    logging.basicConfig(
+        level=logging.INFO,
+        stream=sys.stdout,
+        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s"
+    )
     asyncio.run(main())
