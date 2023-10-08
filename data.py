c4dd818ca8b4332846d42726cac5e78b5f4d2a4f
skreb <skrebnev.dm@gmail.com>
2023-10-08 11:49:38 +0400

    add picters

--- data.py
+++ data.py
@@ -1,9 +1,5 @@
-pictures = {
-    1: "images/picture.jpeg"
-}
-for i in range(2, 51):
-    pictures[i] = f'images/pict_{i}.jpeg'
-print(pictures)
+pictures = {1: 'images/picture.jpeg', 2: 'images/pict_2.jpeg', 3: 'images/pict_3.jpeg', 4: 'images/pict_4.jpeg', 5: 'images/pict_5.jpeg', 6: 'images/pict_6.jpeg', 7: 'images/pict_7.jpeg', 8: 'images/pict_8.jpeg', 9: 'images/pict_9.jpeg', 10: 'images/pict_10.jpeg', 11: 'images/pict_11.jpeg', 12: 'images/pict_12.jpeg', 13: 'images/pict_13.jpeg', 14: 'images/pict_14.jpeg', 15: 'images/pict_15.jpeg', 16: 'images/pict_16.jpeg', 17: 'images/pict_17.jpeg', 18: 'images/pict_18.jpeg', 19: 'images/pict_19.jpeg', 20: 'images/pict_20.jpeg', 21: 'images/pict_21.jpeg', 22: 'images/pict_22.jpeg', 23: 'images/pict_23.jpeg', 24: 'images/pict_24.jpeg', 25: 'images/pict_25.jpeg', 26: 'images/pict_26.jpeg', 27: 'images/pict_27.jpeg', 28: 'images/pict_28.jpeg', 29: 'images/pict_29.jpeg', 30: 'images/pict_30.jpeg', 31: 'images/pict_31.jpeg', 32: 'images/pict_32.jpeg', 33: 'images/pict_33.jpeg', 34: 'images/pict_34.jpeg', 35: 'images/pict_35.jpeg', 36: 'images/pict_36.jpeg', 37: 'images/pict_37.jpeg', 38: 'images/pict_38.jpeg', 39: 'images/pict_39.jpeg', 40: 'images/pict_40.jpeg', 41: 'images/pict_41.jpeg', 42: 'images/pict_42.jpeg', 43: 'images/pict_43.jpeg', 44: 'images/pict_44.jpeg', 45: 'images/pict_45.jpeg', 46: 'images/pict_46.jpeg', 47: 'images/pict_47.jpeg', 48: 'images/pict_48.jpeg', 49: 'images/pict_49.jpeg', 50: 'images/pict_50.jpeg'}
+
 gifs = {
     1: "roach-dancing.mp4"
 }
