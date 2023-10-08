38648d04e1b7d4f39c4e5fce32067a21fb9c2b4d
skreb <skrebnev.dm@gmail.com>
2023-10-08 11:15:42 +0400

    add picters

--- data.py
+++ data.py
@@ -1,6 +1,9 @@
 pictures = {
-    1: "picture.jpeg"
+    1: "images/picture.jpeg"
 }
+for i in range(2, 51):
+    pictures[i] = f'images/pict_{i}.jpeg'
+print(pictures)
 gifs = {
     1: "roach-dancing.mp4"
 }
